b37a596352715509294e8f691ea7bbfa624f8794
Bradley Davis <me@bradleydavis.tech>
2020-03-02 15:45:00 -0800

    Fixed signed/unsigned comparison

--- common/Logging_Test.cpp
+++ common/Logging_Test.cpp
@@ -4,17 +4,17 @@
 
 TEST(Logging, NoSinks) {
   common::logging::configure(nullptr, false);
-  EXPECT_EQ(0, spdlog::default_logger()->sinks().size());
+  EXPECT_EQ(size_t{0}, spdlog::default_logger()->sinks().size());
 }
 
 TEST(Logging, FileSink) {
   common::logging::configure("log.log", false);
-  EXPECT_EQ(1, spdlog::default_logger()->sinks().size());
+  EXPECT_EQ(size_t{1}, spdlog::default_logger()->sinks().size());
 }
 
 TEST(Logging, ConsoleSink) {
   common::logging::configure(nullptr, true);
-  EXPECT_EQ(1, spdlog::default_logger()->sinks().size());
+  EXPECT_EQ(size_t{1}, spdlog::default_logger()->sinks().size());
 }
 
 TEST(Logging, FileAndConsoleSink) {
@@ -24,5 +24,5 @@ TEST(Logging, FileAndConsoleSink) {
   spdlog::warn("warn Test");
   spdlog::error("error Test");
   spdlog::critical("critical Test");
-  EXPECT_EQ(2, spdlog::default_logger()->sinks().size());
+  EXPECT_EQ(size_t{2}, spdlog::default_logger()->sinks().size());
 }
