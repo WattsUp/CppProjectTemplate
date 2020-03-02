15359aca50481515d6df7eab34dfe550047156b9
Bradley Davis <me@bradleydavis.tech>
2020-03-02 13:18:03 -0800

    Added logging tests

--- common/Logging.cpp
+++ common/Logging.cpp
@@ -1,23 +1,32 @@
 #include "Logging.hpp"
 
 #include <spdlog/sinks/rotating_file_sink.h>
-#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <vector>
 
 namespace common {
 namespace logging {
 
+spdlog::sink_ptr sinkFile    = nullptr;
+spdlog::sink_ptr sinkConsole = nullptr;
+
 /**
  * @brief Configure logging with a file, optional console, or neither
  *
- * @param filename of the log file, nullptr for no logging to file
- * @param useConsole will use a console output window if true
+ * @param filename of the log file, nullptr for no logging to file. Configures
+ * level to debug and higher or info if release build
+ * @param useConsole will use a console output window if true. Configures level
+ * to warning and higher
  */
 void configure(const char* filename, bool useConsole) {
+  sinkFile    = nullptr;
+  sinkConsole = nullptr;
   std::vector<spdlog::sink_ptr> sinks;
 
   if (useConsole) {
 #ifdef WIN32
+#ifndef WIN_CONSOLE
+    printf("WIN_CONSOLE set");
     // Attempt to create a console
     if (AllocConsole()) {
       HWND hwnd = GetConsoleWindow();
@@ -29,28 +38,37 @@ void configure(const char* filename, bool useConsole) {
       freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
       freopen_s((FILE**)stderr, "CONOUT$", "w", stderr);
     } else {
-      spdlog::error("Error allocing a console{}", GetLastError());
+      spdlog::error("Error allocing a console: {}", GetLastError());
       throw std::exception("Log console initialization failed");
     }
-#endif
-    sinks.emplace_back(
-        std::make_shared<spdlog::sinks::wincolor_stdout_sink_mt>());
+#endif /* WIN_CONSOLE */
+#endif /* WIN32 */
+
+    sinkConsole = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    sinkConsole->set_level(spdlog::level::warn);
+    sinks.emplace_back(sinkConsole);
   }
 
   // Create a file sink
   if (filename) {
-    sinks.emplace_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
-        filename, MAX_FILE_SIZE, MAX_FILE_COUNT));
+    sinkFile = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+        filename, MAX_FILE_SIZE, MAX_FILE_COUNT);
+#ifdef DEBUG
+    sinkFile->set_level(spdlog::level::debug);
+#else  /* !DEBUG */
+    sinkFile->set_level(spdlog::level::info);
+#endif /* DEBUG */
+    sinks.emplace_back(sinkFile);
   }
 
   std::shared_ptr<spdlog::logger> logger =
       std::make_shared<spdlog::logger>("", begin(sinks), end(sinks));
   spdlog::set_default_logger(logger);
-#ifdef DEBUG
-  spdlog::set_level(spdlog::level::debug);
-#endif
 
-  spdlog::set_pattern("[%P:%t:%m%d/%H:%M:%S.%e][%=8l] %v");
+  spdlog::set_pattern("[%P:%t:%m%d/%H:%M:%S.%e][%^%=7l%$] %v");
+
+  // Set logger level to lowest so the sinks decide which level to log
+  spdlog::set_level(spdlog::level::trace);
 }
 }  // namespace logging
 }  // namespace common
