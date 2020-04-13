2a9a27950b1280a76c0ccb6b758186d1f7b69041
Bradley Davis <me@bradleydavis.tech>
2020-04-13 16:17:17 -0700

    Fixed Linux bugs

--- project-installer/main.cpp
+++ project-installer/main.cpp
@@ -1,4 +1,5 @@
 #include <spdlog/spdlog.h>
+#include <string>
 
 /**
  * @brief Main entry point for program
@@ -8,11 +9,15 @@
 #ifdef WIN32
 int WINAPI WinMain(HINSTANCE /* hInstance */,
                    HINSTANCE /* hPrevInstance */,
-                   char* lpCmdLine,
+                   char* args,
                    int /* nShowCmd */) {
 #else  /* WIN32 */
 int main(int argc, char* argv[]) {
+  std::string argsString = "";
+  for (int i = 0; i < argc; ++i)
+    argsString = argsString + argv[i] + " ";
+  const char* args = argsString.c_str();
 #endif /* WIN32 */
-  spdlog::info("Installing with arguments: {}", lpCmdLine);
+  spdlog::info("Installing with arguments: {}", args);
   return 0;
 }
