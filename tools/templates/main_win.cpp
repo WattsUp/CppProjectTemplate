2cbc799a9284ebedfd93a5d898e919fcfbec2f7e
Bradley Davis <me@bradleydavis.tech>
2020-05-12 12:58:13 -0700

    Added template files for SetupProject

---
+++ tools/templates/main_win.cpp
@@ -0,0 +1,32 @@
+#include "common/Logging.hpp"
+#include "common/Version.h"
+
+#include <Windows.h>
+
+#include <cstdio>
+#include <exception>
+
+/**
+ * @brief Main entry point for program
+ *
+ * @param lpCmdLine command line string
+ * @return int zero on success, non-zero on failure
+ */
+int WINAPI WinMain(HINSTANCE /* hInstance */,
+                   HINSTANCE /* hPrevInstance */,
+                   char* lpCmdline,
+                   int /* nShowCmd */) {
+  try {
+    common::logging::configure("log.log", true);
+  } catch (const std::exception& e) {
+    puts(e.what());
+  }
+#if DEBUG
+  spdlog::info(VERSION_STRING_FULL);
+#else  /* DEBUG */
+  spdlog::info(VERSION_STRING);
+#endif /* DEBUG */
+
+  spdlog::info("Argument: {}", lpCmdline);
+  return 0;
+}
